8f69f3cb71773a5577cb8641b667a8700f2cb076
Bryan <bikeman@mail2000.com.tw>
2020-04-08 15:18:03 +0800

    keep going

--- README.md
+++ README.md
@@ -3,3 +3,30 @@ from firebase_admin import credentials
 
 
 
+from firebase-admijn import firestore
+
+db = firestore.client()
+
+db.collection().document()
+
+aibigdata-107598064 <== project id
+
+aibigdata-107598064-271403 <== firebase id
+
+
+
+
+sudo apt install python3-opencv
+
+
+import cv2
+
+import os
+
+
+cv2.VideoCapture
+
+.set(1, <frame>)
+.imwrite(<string>)
+
+acl <== access control list
--- test.py
+++ test.py
@@ -1,28 +1,45 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from firebase_admin import storage
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+# from firebase_admin import storage
+
+import cv2
+
+
+# keyFile = 'aibigdata.json'
+
+# storageBucket = 'aibigdata-107598064-271403.appspot.com'
+
+
+# cred = credentials.Certificate(keyFile)
+
+# firebase_admin.initialize_app(cred, {'storageBucket': storageBucket})
+
+# client = firestore.client()
+
+# bucket = storage.bucket()
+
+# blob = bucket.blob('test/aibigdata.json')
+# blob.upload_from_filename(keyFile)
+# blob.make_public()
 
 
 
-keyFile = 'aibigdata.json'
+# db = firestore.client()
 
-storageBucket = 'aibigdata-107598064-271403.appspot.com'
-
-
-cred = credentials.Certificate(keyFile)
-
-firebase_admin.initialize_app(cred, {'storageBucket': storageBucket})
-
-client = firestore.client()
-
-bucket = storage.bucket()
-
-blob = bucket.blob('test/aibigdata.json')
-blob.upload_from_filename(keyFile)
-blob.make_public()
+# db.collection('test').document().set({'aaa':123, 'bbb':222})
 
 
 
 
-
+vc = cv2.VideoCapture('Sample.mp4')
+num  = vc.get(cv2.CAP_PROP_FPS)
+print(num)
+ret , frame  = vc.read()
+cv2.imwrite('test.png', frame)
+for i in range(10):
+    print(i * int(num))
+    vc.set(1, int(num) * i)
+    ret , frame  = vc.read()
+    cv2.imwrite(str(i) + '.png', frame)
+    
